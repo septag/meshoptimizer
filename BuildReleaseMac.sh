cdeea35edf345deec67b5f1fd738b8c4012dc91d
septag <septag@pm.me>
2025-02-15 17:48:11 +0100

    Add build script for Mac-arm64

---
+++ BuildReleaseMac.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+mkdir -p build
+cd build
+
+cmake -DMESHOPT_BUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+
+make
+
+mkdir -p dist
+mkdir -p dist/lib/mac-arm64
+cp -f libmeshoptimizer.dylib dist/lib/mac-arm64
+
+mkdir -p dist/include
+cp -f ../src/meshoptimizer.h dist/include
+
+cd dist && zip -r ../meshopt_dist-mac-arm64.zip ./*
+
+cd ..
+
